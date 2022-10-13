936452e9907fe447e20857a6ff5913c792b46075
thales-pinner <thales.neves@pinpeople.com.br>
2022-10-12 22:19:06 -0300

    Storybook setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import './styles/global.css';
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Hello World</h1>
+
+      <button className="bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600">
+        Send
+      </button>
+    </>
   )
 }
 
-export default App
